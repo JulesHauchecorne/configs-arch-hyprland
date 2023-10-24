f8c44e24fc3ced7e9cfb22e621fd2c9aa6679760
Jules Hauchecorne <youle21@yahoo.ca>
2023-10-24 14:35:09 -0400

    maj

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+function fish_greeting
+end
+
+fish_vi_key_bindings
+
+alias v "nvim"
+
+alias record "wf-recorder"
+alias lbgsakura "pkill mpvpaper; pkill swaybg;  mpvpaper '*' -o '--loop --no-audio' /home/jules/Pictures/backgrounds/live-wallpaper/sakura2.mp4"
+alias lbgring="pkill mpvpaper; pkill swaybg;  mpvpaper '*' -o '--loop --no-audio' /home/jules/Pictures/backgrounds/live-wallpaper/bluering.mp4" 
+alias lbgspace="pkill mpvpaper; pkill swaybg;  mpvpaper '*' -o '--loop --no-audio' /home/jules/Pictures/backgrounds/live-wallpaper/space.mp4" 
+alias bgprettyface=" pkill mpvpaper; pkill swaybg; swaybg -m fill -i ~/Pictures/backgrounds/pretty_girl.jpg"
+
