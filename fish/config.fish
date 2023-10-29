1bd4780bc66796189bf97f3c683c3264e08f2ddc
Jules Hauchecorne <youle21@yahoo.ca>
2023-10-29 11:51:19 -0300

    maj

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,7 @@ end
 fish_vi_key_bindings
 
 alias v "nvim"
-
+alias mvnnew "mvn archetype:generate -DgroupId=ToolsQA -DartifactId=MavenProject -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false"
 alias record "wf-recorder"
 alias lbgsakura "pkill mpvpaper; pkill swaybg;  mpvpaper '*' -o '--loop --no-audio' /home/jules/Pictures/backgrounds/live-wallpaper/sakura2.mp4"
 alias lbgring="pkill mpvpaper; pkill swaybg;  mpvpaper '*' -o '--loop --no-audio' /home/jules/Pictures/backgrounds/live-wallpaper/bluering.mp4" 
