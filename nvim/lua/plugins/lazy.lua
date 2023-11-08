9b50ae19079690ec64417a32ab898743d0a9e238
Jules Hauchecorne <youle21@yahoo.ca>
2023-11-07 20:48:09 -0400

    maj

--- nvim/lua/plugins/lazy.lua
+++ nvim/lua/plugins/lazy.lua
@@ -59,7 +59,7 @@ return {
             "hrsh7th/cmp-path",
             "hrsh7th/cmp-cmdline",
             "Jezda1337/cmp_bootstrap",
-           "hrsh7th/cmp-nvim-lsp-signature-help",
+            "hrsh7th/cmp-nvim-lsp-signature-help",
             "L3MON4D3/LuaSnip",
             "hrsh7th/cmp-vsnip",
         },
--- nvim/lua/settings/remap.lua
+++ nvim/lua/settings/remap.lua
@@ -67,3 +67,5 @@ vim.keymap.set('n', '<C-e>', ':lua require("harpoon.ui").toggle_quick_menu()<CR>
 vim.keymap.set('n', '<C-h>', ':lua require("harpoon.ui").nav_file(1)<CR>', opts)
 vim.keymap.set('n', '<C-n>', ':lua require("harpoon.ui").nav_file(2)<CR>', opts)
 vim.keymap.set('n', '<C-t>', ':lua require("harpoon.ui").nav_file(3)<CR>', opts)
+
+
