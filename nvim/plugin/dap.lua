787d6b3d5bf6cec2e8951bcd61759eede8fad9dc
Jules Hauchecorne <Jules.Hauchecorne@fdmgroup.com>
2023-11-21 21:00:52 -0500

    maj

--- nvim/plugin/dap.lua
+++ nvim/plugin/dap.lua
@@ -1,4 +1,9 @@
 vim.keymap.set('n', '<leader>dl', ':DapLoadLaunchJSON<CR>', opts )
+
+--  REALLY A PAIN TO MAKE THIS WORK , DO NOT REMOVE LINE BELOW, copy modified version according to needs
+ -- custom command to load custom config args
+vim.cmd("nnoremap <leader>dL <Cmd>lua require('jdtls.dap').setup_dap_main_class_configs({ config_overrides = { vmArgs = '-Dspring.config.location=\"/home/jules/data.properties\"'} })<CR>")
+
 vim.cmd("nnoremap <leader>dd <Cmd>lua require'dapui'.toggle()<CR>")
 vim.keymap.set('n', '<leader>dt', ':DapToggleRepl<CR>', opts )
 vim.cmd("nnoremap <leader>dc <Cmd>lua require'dap'.continue()<CR>")
